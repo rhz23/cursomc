b5d1e306a2f8d3ad83c796bc80e1572309773167
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-09-19 00:40:14 -0300

    Produto class and association Many to Many

--- src/main/java/com/rzworks/cursomc/CursomcApplication.java
+++ src/main/java/com/rzworks/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.rzworks.cursomc;
 
 import com.rzworks.cursomc.domain.Categoria;
+import com.rzworks.cursomc.domain.Produto;
 import com.rzworks.cursomc.repositories.CategoriaRepository;
+import com.rzworks.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,8 @@ public class CursomcApplication implements CommandLineRunner {
     //cria uma dependencia com a classe "categoriaRepository" de forma a ter acessar e utilizar os recursos da classe de repositório
     @Autowired
     private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +30,19 @@ public class CursomcApplication implements CommandLineRunner {
         Categoria cat1 =  new Categoria(null, "Informática");
         Categoria cat2 =  new Categoria(null, "Escritório");
 
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+        
+        cat1.getProdutos().addAll(Arrays.asList(p1,p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
     }
 }
---
+++ src/main/java/com/rzworks/cursomc/domain/Produto.java
@@ -0,0 +1,78 @@
+package com.rzworks.cursomc.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Double preco;
+
+    @ManyToMany
+    @JoinTable(name = "PRODUTO_CATEGORIA",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public Produto(){
+    }
+
+    public Produto(Integer id, String name, Double preco) {
+        this.id = id;
+        this.name = name;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
